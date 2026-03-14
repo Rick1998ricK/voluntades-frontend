b8ee96b0bfdf12dcbbc009001366ad8cb8e2c668
Ricardo Romero <rrick8757@gmail.com>
2026-03-14 09:39:52 -0500

    fix: add use client to users id page

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,1 +1,8 @@
-//No es necesario el show del usuario, eso se vera en VOLUNTEERS.
+"use client";
+
+import React from "react";
+
+const UserPage = () => <div>Página no implementada</div>;
+// Esta página no se usará, se maneja en VOLUNTEERS
+
+export default UserPage;
